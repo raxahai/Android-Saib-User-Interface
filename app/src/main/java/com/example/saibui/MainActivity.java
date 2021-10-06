fd12ac468f4a1e3246fc3f2bb79de83c048144c0
Syed Raza Haider <Raza.Haider@tpsonline.com>
2021-10-06 21:00:41 +0500

    Added layout and screenshot

--- app/src/main/java/com/example/saibui/MainActivity.java
+++ app/src/main/java/com/example/saibui/MainActivity.java
@@ -2,13 +2,26 @@ package com.example.saibui;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+    private Button loginButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        loginButton = findViewById(R.id.loginButton);
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this,LoginActivity.class);
+                startActivity(intent);
+            }
+        });
     }
+
 }
--- readme.md
+++ readme.md
@@ -5,3 +5,4 @@ My first complete android Ui design for practicing basis.
 Source images attached below:
 
 <img src="/images/Screenshot_1632577626.png" alt="/images/Screenshot_1632577626.png" width="300"/>
+<img src="/images/Screenshot_1633535750.png" alt="/images/Screenshot_1633535750.png" width="300"/>
